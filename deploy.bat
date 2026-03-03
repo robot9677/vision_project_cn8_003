12a7cbf264d5780225cf64cbbd15668e40aea97e
robot96 <robot96@192.168.0.14>
2026-03-03 19:34:37 +0900

    auto deploy

---
+++ deploy.bat
@@ -0,0 +1,12 @@
+@echo off
+
+git add .
+git commit -m "auto deploy"
+git push origin main
+
+ssh -i %USERPROFILE%\.ssh\deploy_for_gha robot96@192.168.0.14 ^
+"cd ~/vision_project && git fetch origin && git reset --hard origin/main && git rev-parse --short HEAD"
+
+echo.
+echo ===== DEPLOY COMPLETE =====
+pause
--- deploy.ps1
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m "auto deploy"
-git push origin main
-ssh -i ~/.ssh/deploy_for_gha robot96@192.168.0.14 "cd ~/vision_project && git fetch origin && git reset --hard origin/main && git rev-parse --short HEAD"
