d732e7da0c91cc4a7ae2ce76af303dff1fb1cd9d
robot96 <robot96@192.168.0.14>
2026-03-03 20:00:35 +0900

    auto deploy

--- deploy.bat
+++ deploy.bat
@@ -5,7 +5,7 @@ git commit -m "auto deploy"
 git push origin main
 
 ssh -i %USERPROFILE%\.ssh\deploy_for_gha robot96@192.168.0.14 ^
-"cd ~/vision_project && git fetch origin main && git reset --hard origin/main && git rev-parse --short HEAD && git log -1 --oneline"
+"cd ~/vision_project && git fetch origin main && git reset --hard origin/main && sudo systemctl restart vision && sleep 1 && systemctl status vision --no-pager | head -n 15"
 echo.
 echo ===== DEPLOY COMPLETE =====
 pause
