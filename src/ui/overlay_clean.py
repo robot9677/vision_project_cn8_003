fa2dbfad4d92a76dcbfa84c088b49b99626eb984
robot96 <robot96@192.168.0.14>
2026-03-16 14:00:53 +0900

    auto deploy

--- src/ui/overlay_clean.py
+++ src/ui/overlay_clean.py
@@ -64,8 +64,8 @@ def draw_rois(img, rois=None, active_id=None, roi_results=None, show_only_select
     line_spacing = 4
 
     roi_text_color = (0, 255, 0)
-    roi_text_scale = 0.4
-    roi_text_thickness = 1
+    roi_text_scale = 0.3
+    roi_text_thickness = 0.5
 
     for idx, r in enumerate(rois):
         # normalize roi dict/object to {id, x,y,w,h, name/label}
@@ -196,7 +196,7 @@ def draw_rois(img, rois=None, active_id=None, roi_results=None, show_only_select
                 draw_text(
                     img,
                     t,
-                    (tx, ty + (i * 14)),
+                    (tx, ty + (i * 12)),
                     color=roi_text_color,
                     scale=roi_text_scale,
                     thickness=roi_text_thickness,
