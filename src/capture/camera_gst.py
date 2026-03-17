0c39435697fc686c87136e46c91230aba2d843d1
robot96 <robot96@192.168.0.14>
2026-03-17 17:17:41 +0900

    auto deploy

--- src/capture/camera_gst.py
+++ src/capture/camera_gst.py
@@ -34,7 +34,7 @@ class CameraGST:
         patterns=None,
         # denoise/hotpixel options
         denoise_method: str = 'median',   # 'none','median','nlmeans','hotpixel'
-        median_ksize: int = 3,
+        median_ksize: int = 1,
         nlm_h: float = 2.0,
         hp_collect_frames: int = 10,
         hp_thresh: float = 10.0,
